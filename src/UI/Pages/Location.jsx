80ce3be4d15affd0c73e71a256424d7a9225f3d8
nithish309 <148962856+nithish309@users.noreply.github.com>
2025-04-29 11:58:33 +0530

    Update Location.jsx

--- src/UI/Pages/Location.jsx
+++ src/UI/Pages/Location.jsx
@@ -5,7 +5,7 @@ const Location = () => {
     <div className="flex justify-center items-center  w-full h-screen">
         <iframe
           className="w-full h-full"
-          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d236.98423061730637!2d77.60037915067473!3d11.286220818339775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTHCsDE3JzEwLjQiTiA3N8KwMzYnMDEuOSJF!5e1!3m2!1sen!2sin!4v1729344246523!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3912.832318680479!2d77.57750277481253!3d11.273733588906838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba96dd9d1035e39%3A0x819424e50d27e4ee!2sPerundurai%20New%20bus%20stand!5e0!3m2!1sen!2sin!4v1745907951750!5m2!1sen!2sin"
         ></iframe>
     </div>
   );
